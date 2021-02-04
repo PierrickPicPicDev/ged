6d9438f7f83462d1d589c1b612731fea0fbfa4b2
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2021-02-04 22:26:38 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import {
+  domain,
+  count,
+  prettyDate,
+  pluralize,
+} from './filters';
 
 Vue.config.productionTip = false;
+
+Vue.filter('count', count);
+Vue.filter('domain', domain);
+Vue.filter('prettyDate', prettyDate);
+Vue.filter('pluralize', pluralize);
 
 new Vue({
   router,
